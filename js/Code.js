32d24ec01b4c066ac82365c700c12488d5fa30d0
SamhitaG <72413840+SamhitaG@users.noreply.github.com>
2021-06-12 11:59:31 -0500

    Add files via upload

---
+++ js/Code.js
@@ -0,0 +1,32 @@
+const accessCode1 = "VARIABLE";
+const accessCode2 = "FUNCTION";
+const accessCode3 = "DATABASE";
+
+function clues() {
+    
+
+    fill("white") 
+    textSize(17)
+    text("Description: Unscramble the words to get the Treasure !!!!", 540, 80)
+
+
+
+    fill("white")
+    textSize(15)
+    text("R E V B A I L A", 100,50)
+    fill("lightblue")
+    text("Hint: Always changing, not constant", 100,70)
+
+    fill("white")
+    textSize(15)
+    text("C U T N I F O N", 650,150)
+    fill("lightblue")
+    text("Hint: It performs a particular task!", 700,170)
+
+    fill("white")
+    textSize(15)
+    text("A T E D A S B A", 100,250)
+    fill("lightblue")
+    text("Hint: what is something that stores information?", 100,270)
+
+}
